fd41ddf6c394a11eede8ad6aedaa14458fc9659b
Ollaberganov-Mirzoxid <ollaberganovmirzoxid@gmail.com>
2024-04-20 02:20:57 +0500

    Frontend yuklandi

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let popur = document.getElementById("img-title-view");
+
+function openPopur(){
+    popur.classList.add("open-view");
+}
+
+function closePopur(){
+    popur.classList.remove("open-view");
+}
