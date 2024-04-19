589bec7ef53fe10fefc96e874b372998d39ecbed
Ollaberganov-Mirzoxid <ollaberganovmirzoxid@gmail.com>
2024-04-20 04:02:19 +0500

    yangilangan frontend

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,13 @@ function openPopur(){
 function closePopur(){
     popur.classList.remove("open-view");
 }
+
+let popur2 = document.getElementById("img-title-view2");
+
+function openPopur2(){
+    popur2.classList.add("open-view");
+}
+
+function closePopur2(){
+    popur2.classList.remove("open-view");
+}
